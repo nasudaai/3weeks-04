f6b34e0d7f68c20df27f2bb979985aa9fbd30396
nasuda <nasuda908@gmail.com>
2026-04-21 08:55:26 +0900

    add write file

--- first/yen.mjs
+++ first/yen.mjs
@@ -1,3 +1,5 @@
+import { writeFileSync } from 'node:fs';
+
 const args = process.argv.slice(2);
 console.log(args);
 
@@ -12,3 +14,6 @@ console.log(data.yen);
 
 const type = typeof args[0];
 console.log(type);
+
+
+writeFileSync("out/yen.txt", args[0]);
