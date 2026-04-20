7618741434363efc3c6dcaa35d3ae21b1040cd41
nasuda <nasuda908@gmail.com>
2026-04-20 23:52:14 +0900

    add obj

--- first/yen.mjs
+++ first/yen.mjs
@@ -1,2 +1,14 @@
 const args = process.argv.slice(2);
 console.log(args);
+
+const data = {
+  yen: args[0],
+}
+
+console.log(data);
+
+console.log(args[0]);
+console.log(data.yen);
+
+const type = typeof args[0];
+console.log(type);
