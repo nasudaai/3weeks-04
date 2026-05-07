037782be67082d8336afd0c1d9e799e3136b1795
nasuda <nasuda908@gmail.com>
2026-05-07 16:35:18 +0900

    adjust cond

--- third/main.mjs
+++ third/main.mjs
@@ -1,14 +1,18 @@
 const args = process.argv.slice(2);
 console.log(args);
 
-const mode = args.length === 1 ? "mode" : "args";
+const mode = (args.length > 1 && args[0] === 'obj') ? "obj" : "args";
 console.log(mode);
 
 const data = {};
 
 if ( mode === "obj" ) {
-  obj[args[1]] = "value";
-  console.log(obj);
+  for (let i = 1; i < args.length; i++) {
+
+    data[args[i]] = "value";
+  }
+
+  console.log(data);
 }
 
 function main() {
