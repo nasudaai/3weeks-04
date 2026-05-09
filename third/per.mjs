c49a8a6322fc281c3aded6326ee0646789b58ec5
nasuda <nasuda908@gmail.com>
2026-05-09 20:24:17 +0900

    update

--- third/per.mjs
+++ third/per.mjs
@@ -1,2 +1,5 @@
+const miss = 1;
+const total = 100;
+
 const per = 1 / 100 * 100;
 console.log(per, "%");
--- third/write.mjs
+++ third/write.mjs
@@ -1,5 +1,7 @@
 import { writeFileSync } from 'node:fs';
 console.log(writeFileSync);
 
+const memo = "memo";
+
 writeFileSync("out/memo.md", "memo");
 
